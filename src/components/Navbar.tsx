d1fee09a77074299b7085377449ab6a387a1ab49
TejasThombare20 <img_2020066@iiitm.ac.in>
2023-12-14 04:29:36 +0530

    update Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,12 @@
-"use client"
-import MaxWidthWrapper from "./MaxWidthWrapper"
-import Link from "next/link"
-import { buttonVariants } from "./ui/button"
-import { ArrowRight } from "lucide-react"
+"use client";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { ArrowRight } from "lucide-react";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
-
 const Navbar = () => {
-
   const { data: session } = useSession();
   const isUserLoggedIn = true;
   const [provider, setprovider] = useState(null);
@@ -21,72 +19,66 @@ const Navbar = () => {
     setupProviders();
   }, []);
 
- 
-
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all  ">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b  border-zinc-200 ">
-          <Link
-            href='/'
-            className="flex z-40 font-semibold ">
+          <Link href="/" className="flex z-40 font-semibold ">
             <span>QueryQuill</span>
           </Link>
 
-          <div className="hidden items-center space-x-4 sm:flex " >
+          <div className="hidden items-center space-x-4 sm:flex ">
             <>
               <Link
-                href='/pricing'
+                href="/pricing"
                 className={buttonVariants({
                   variant: "ghost",
-                  size: 'sm'
-                })}>
+                  size: "sm",
+                })}
+              >
                 Pricing
               </Link>
 
-              {
-                session?.user ? (
-                  <button
-                    className={buttonVariants({
-                      variant: "ghost",
-                      size: 'sm'
-                    })}
-                    onClick={signOut}
-                  >
-                    Sign Out
-                  </button>
-                ) : (
-                  <>
-
-                    {provider &&
-                      Object.values(provider).map((provider) => (
-                        <button
-                          type="button"
-                          key={provider.name}
-                          onClick={() => signIn(provider.id)}
-                          className="rounded-full border border-black bg-black py-1.5 px-5 text-white transition-all hover:bg-white hover:text-black text-center text-sm font-inter flex items-center justify-center "
-                        >
-                          Sign In
-                        </button>
-                      ))}
-                  </>
-                )
-              }
+              {session?.user ? (
+                <button
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                  onClick={() => signOut({ callbackUrl: "/" })}
+                >
+                  Sign Out
+                </button>
+              ) : (
+                <>
+                  {provider &&
+                    Object.values(provider).map((provider) => (
+                      <button
+                        type="button"
+                        key={provider.name}
+                        onClick={() => signIn(provider.id)}
+                        className="rounded-full border border-black bg-black py-1.5 px-5 text-white transition-all hover:bg-white hover:text-black text-center text-sm font-inter flex items-center justify-center "
+                      >
+                        Sign In
+                      </button>
+                    ))}
+                </>
+              )}
 
               <Link
-                href='/signup'
+                href="/signup"
                 className={buttonVariants({
-                  size: 'sm'
-                })}>
+                  size: "sm",
+                })}
+              >
                 Get Started <ArrowRight className="ml-1.5 h-4 w-4  " />
               </Link>
             </>
           </div>
-
         </div>
       </MaxWidthWrapper>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
